c7c183bcd43469d5073bd4685d2d17cf3065b440
eduardo diaz <eduardodiaz06@gmail.com>
2023-01-21 21:59:36 -0300

    jsentregafinal

---
+++ index.js
@@ -0,0 +1,82 @@
+const personajesHtml = (array) => {
+    const arrayReducido = array.reduce((acc, elemento) => {
+        return acc + `
+        <article>
+            
+            <div class="image-container">
+                <img src="${elemento.image}" alt="Personaje">
+            </div>
+            <h2>${elemento.name}</h2>
+            <p>Estado: ${elemento.status}</p>
+            <p>Especie: ${elemento.species}</p>
+            <p>Genero: ${elemento.gender}</p>
+
+        </article>
+        `
+    }, "")
+
+    document.querySelector(".contenedor").innerHTML = arrayReducido
+}
+
+
+
+fetch("https://rickandmortyapi.com/api/character")
+    .then((respuesta) => respuesta.json())
+    .then((data) => {
+        const array = data.results
+        console.log(array)
+        personajesHtml(data.results)
+    })
+    .catch((error) => console.log("Algo salió mal"))
+
+
+
+const form = document.querySelector("form")
+
+
+
+form.onsubmit = (e) => {
+    e.preventDefault()
+swal({
+    text: 'Busca el nombre de tu personaje favorito',
+    content: "input",
+    button: {
+        text: "Buscar",
+        closeModal: false,
+    },
+})
+    .then(name => {
+        if (!name) throw null;
+
+        return fetch(`https://rickandmortyapi.com/api/character/?name=${name}`);
+    })
+    .then(results => {
+        return results.json();
+    })
+    .then(json => {
+        const personaje = json.results[0];
+
+        if (!personaje) {
+            return swal("Personaje no encontrado");
+        }
+
+        const name = personaje.name;
+        const imageURL = personaje.image;
+
+        swal({
+            title: "Resultados",
+            text: name,
+            icon: imageURL,
+
+        });
+    })
+    .catch(err => {
+        if (err) {
+            swal("Oh no!", "Personaje no encontrado");
+        } else {
+            swal.stopLoading();
+            swal.close();
+        }
+    });
+
+}
